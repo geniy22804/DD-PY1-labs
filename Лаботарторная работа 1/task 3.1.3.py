96bd71db5b32731813c851d02222a8543d939f4f
Вадим <afternoon.4865@gmail.com>
2025-04-24 21:57:52 +0300

    Lab 1

---
+++ Лаботарторная работа 1/task 3.1.3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+np.random.seed(42)
+vector = np.random.random(10)
+sorted_vector = np.sort(vector)
+
+print(sorted_vector)
---
+++ Лаботарторная работа 1/task 3.1.4.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+array = np.zeros(6, dtype=int)
+array[-1] = 1
